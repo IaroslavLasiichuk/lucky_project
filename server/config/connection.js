36a3b15d749b0f7f6328715fce0f90cbe9883901
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2025-04-06 21:26:22 -0600

    Mobgo Connected

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,12 @@ const config = require('./config');
 require("dotenv").config();
 
 
-mongoose.connect(config.mongoURI);
-
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('✅ MongoDB Connected!'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
