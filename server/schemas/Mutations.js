06333e04782b4d67ac35c8246e1754e1787a3338
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2025-04-08 21:43:36 -0600

    Adds Mutations

---
+++ server/schemas/Mutations.js
@@ -0,0 +1,19 @@
+// Import the User model from the "models" module.
+const { User, Job } = require("../models");
+
+
+const Mutations = {
+    addJob: async (
+      parent,
+      {  },
+      context
+    ) => {
+      try {
+      
+      } catch (error) {
+       
+    }
+    }
+  }
+
+  module.exports = Mutations
